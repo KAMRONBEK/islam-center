6dc35fd7f724e396d6f5060cf191fbf0d7102a5f
mmirkhalikoff <mirjalol.mirm.m@gmail.com>
2022-03-23 15:10:23 +0500

    add user details

--- src/view/components/Drawer/CostumDrawer.tsx
+++ src/view/components/Drawer/CostumDrawer.tsx
@@ -12,9 +12,19 @@ import {
   DrawerItemList,
 } from '@react-navigation/drawer';
 import {windowHeight, windowWidth} from '../../constants/size';
+import {
+  FacebookIcon,
+  InstagramIcon,
+  Pattern,
+  TelegrammIcon,
+} from '../../assets/icons/icon';
+import {colors} from '../../theme';
+import {useNavigation} from '@react-navigation/native';
 
 //@ts-ignore
 const CustomDrawer = (props, index) => {
+  let navigation = useNavigation();
+
   return (
     <View style={{flex: 1, width: windowWidth / 1 - 25}}>
       <DrawerContentScrollView
@@ -24,6 +34,7 @@ const CustomDrawer = (props, index) => {
         contentContainerStyle={{
           // backgroundColor: '#8200d6',
           width: windowWidth / 1 - 25,
+          zIndex: 999,
           // paddingBottom: 100,
           // position: 'relative',
         }}>
@@ -31,29 +42,109 @@ const CustomDrawer = (props, index) => {
           source={require('../../assets/images/details_header_bg.png')}
           resizeMode="cover"
           style={{width: windowWidth / 1, height: 200}}>
-          <Text
+          <View
             style={{
-              color: '#fff',
-              fontSize: 18,
-              //   fontFamily: 'Roboto-Medium',
-              marginBottom: 5,
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+              paddingVertical: 20,
             }}>
-            John Doe
-          </Text>
-          <View style={{flexDirection: 'row'}}>
-            <Text
+            <View
               style={{
-                color: '#fff',
-                // fontFamily: 'Roboto-Regular',
-                marginRight: 5,
+                width: windowWidth / 1,
+                flexDirection: 'row',
+                justifyContent: 'flex-end',
+                paddingHorizontal: 20,
               }}>
-              280 Coins
-            </Text>
-            {/* <FontAwesome5 name="coins" size={14} color="#fff" /> */}
+              <TouchableOpacity onPress={() => navigation.closeDrawer()}>
+                <Text
+                  style={{
+                    fontSize: 23,
+                    fontWeight: '600',
+                    lineHeight: 35,
+                    letterSpacing: 1,
+                    color: colors.white,
+                    // width: 220,
+                    marginHorizontal: 25,
+                  }}>
+                  X
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'flex-start',
+                alignItems: 'center',
+                // width: '100%',
+                paddingHorizontal: 25,
+              }}>
+              <TouchableOpacity>
+                <View
+                  style={{
+                    width: 86,
+                    height: 86,
+                    backgroundColor: '#991',
+                    borderRadius: 50,
+                  }}></View>
+              </TouchableOpacity>
+              <View
+                style={{
+                  flexDirection: 'column',
+                  alignItems: 'flex-start',
+                  justifyContent: 'space-around',
+                }}>
+                <Text
+                  style={{
+                    fontSize: 23,
+                    fontWeight: '600',
+                    lineHeight: 35,
+                    letterSpacing: 1,
+                    color: colors.white,
+                    // width: 220,
+                    paddingHorizontal: 24,
+                  }}>
+                  Ройтман Рафаэль
+                </Text>
+                <TouchableOpacity>
+                  <Text
+                    style={{
+                      fontSize: 18,
+                      fontWeight: '600',
+                      lineHeight: 35,
+                      letterSpacing: 1,
+                      color: colors.gray,
+                      // width: 220,
+                      paddingHorizontal: 24,
+                    }}>
+                    Посмотреть профиль
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View
+              style={{
+                width: windowWidth / 1,
+                flexDirection: 'row',
+                justifyContent: 'flex-end',
+                paddingHorizontal: 20,
+              }}>
+              <Text
+                style={{
+                  fontSize: 23,
+                  fontWeight: '600',
+                  lineHeight: 35,
+                  letterSpacing: 1,
+                  color: colors.white,
+                  marginHorizontal: 25,
+                }}>
+                RU
+              </Text>
+            </View>
           </View>
         </ImageBackground>
 
-        <View style={{height: windowHeight / 2 + 60}}>
+        <View style={{height: windowHeight / 2 + 75}}>
           <ScrollView
             showsVerticalScrollIndicator={false}
             contentContainerStyle={{
@@ -77,28 +168,34 @@ const CustomDrawer = (props, index) => {
           borderTopWidth: 1,
           borderTopColor: '#ccc',
         }}>
-        <TouchableOpacity onPress={() => {}} style={{}}>
-          <View style={{flexDirection: 'row', alignItems: 'center'}}>
-            <Text
-              style={{
-                fontSize: 15,
-                marginLeft: 5,
-              }}>
-              Tell a Friend
-            </Text>
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => {}} style={{paddingVertical: 15}}>
-          <View style={{flexDirection: 'row', alignItems: 'center'}}>
-            <Text
-              style={{
-                fontSize: 15,
-                marginLeft: 5,
-              }}>
-              Sign Out
-            </Text>
-          </View>
-        </TouchableOpacity>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-around',
+            width: windowWidth / 3 + 20,
+            paddingBottom: 20,
+          }}>
+          <TouchableOpacity onPress={() => {}} style={{}}>
+            <FacebookIcon size={25} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {}} style={{}}>
+            <TelegrammIcon size={25} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {}} style={{}}>
+            <InstagramIcon size={25} />
+          </TouchableOpacity>
+        </View>
+        <View
+          style={{
+            position: 'absolute',
+            marginTop: -40,
+            bottom: -60,
+            right: -0,
+            zIndex: -1,
+          }}>
+          <Pattern size={200} />
+        </View>
       </View>
     </View>
   );
