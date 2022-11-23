5c6f85d7f255cd89a28b238eb3c5f4a85fbd6577
Diyorbek Developer <xidirovdiyorbek6@gmail.com>
2022-11-23 14:30:07 +0500

    aa

--- src/context/allapi/AllApiContext.tsx
+++ src/context/allapi/AllApiContext.tsx
@@ -13,12 +13,12 @@ export const useAllApiContext = () => {
 export const AllApiContext: React.FC<React.ReactNode> = ({children}) => {
   // ------------------GET-------------------
   // NEW POST -------------
-  const [newPosts, setNewPost] = useState<any>([]) ;
+  const [newPosts, setNewPost] = useState<any>([]);
   function getNewPosts() {
     axios
       .get(`${API_URL}news_press_services`)
       .then(res => {
-        setNewPost(res.data); 
+        setNewPost(res.data);
       })
       .catch(res => {
         console.log(`Error New Post - ${res}`);
